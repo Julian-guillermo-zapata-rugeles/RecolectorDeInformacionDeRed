6cfd4115baeac9439921c64a4982ec61fad601cc
static <latanic@colossus.static>
2021-04-29 17:20:11 -0500

    se añadió el modulo de escritura que escribirá la información

---
+++ modulo_escritura.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+
+def crearCarpeta():
+    #creacion de la carpeta analiticas
+    os.system("mkdir ANALITICAS 2>/dev/null")
+
+
+def escribirArchivo(lista_elementos):
+    # se almacenará la información necesaria en el archivo analiticas que se encuentre presente
